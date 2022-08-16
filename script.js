6e40b73cd5d068eb5560e4ec2028c62d9bef0e00
mukhammedjanov-web <akmalmuhammadjonov6@gmail.com>
2022-08-17 01:30:45 +0500

    3-month homework-4

---
+++ script.js
@@ -0,0 +1,190 @@
+// HOMEWORK LESSON-4
+
+
+//*********** 1-m **********
+
+// let k=+prompt('k=');
+// let n=+prompt('n=');
+
+// for (let i=1; i<=n; i++ ){
+//      console.log(k)
+// }
+
+
+//*********** 2-m **********
+
+// let a=+prompt('"A" son kiriting');
+// let b=+prompt('"B" son kiriting ("A" sonidan kichik bolsin) ');
+
+// for (let i=b; i<=a; i++ ){
+//      console.log(i)
+// }
+
+
+//*********** 3-m **********
+
+// let a=+prompt('"A" son kiriting');
+// let b=+prompt('"B" son kiriting ("A" sonidan kichik bolsin) ');
+
+// for (let i = a; i > b; i-- ){
+//           if (i===a)
+//           continue
+//      console.log(i)
+// }
+
+
+//*********** 4-m **********
+
+// for (let i = 1; i <= 10; i++ ){
+//      console.log (`${i}kg shakar narxi ${i*10000}`);
+// }
+
+
+//*********** 5-m **********
+
+// for (let i = 1; i <= 10; i++ ){
+//      console.log (`${i/10}kg shakar narxi ${i*10000/10}`);
+// }
+
+
+//*********** 6-m **********
+
+// for (let i = 12; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     console.log(`${i / 10}kg shakar narxi ${(i * 10000) / 10}`);
+//   }
+// }
+
+
+//*********** 7-m **********
+
+// let a=+prompt('"A" son kiriting');
+// let b=+prompt('"B" son kiriting ("A" sonidan katta bolsin) ');
+// let sum=0;
+
+// for (let i = a; i <= b; i++ ){
+//      sum+=i        
+// }
+// console.log(`A va B sonlar orasidagi butun sonlar yigindisi ${sum} ga teng`);
+
+
+//*********** 8-m **********
+
+// let a=+prompt('"A" son kiriting');
+// let b=+prompt('"B" son kiriting ("A" sonidan katta bolsin) ');
+// let sum=1;
+
+// for (let i = a; i <= b; i++ ){
+//      sum*=i        
+// }
+// console.log(`A va B sonlar orasidagi butun sonlar ko'paytmasi ${sum} ga teng`);
+
+
+//*********** 9-m **********
+
+// let a=+prompt('"A" son kiriting');
+// let b=+prompt('"B" son kiriting ("A" sonidan katta bolsin) ');
+// let sum=0;
+
+// for (let i = a; i <= b; i++ ){
+//      sum+=i**2        
+// }
+// console.log(`A va B sonlar orasidagi butun sonlar kvadratlari yig'indisi ${sum} ga teng`);
+
+
+//*********** 10-m **********
+
+// let n=+prompt('"N" son kiriting');
+// let sum=0;
+
+// for (let i = 1; i <= n; i++ ){
+//      sum+=i*(1/i**2)        
+// }
+// console.log(`Yig'indi ${sum} ga teng`);
+
+
+//*********** 11-m **********
+
+// let n=+prompt('"N" son kiriting');
+
+// for (let i = 1; i <= n; i++ ){
+//      if(i%2==1)
+//      console.log(i);        
+// }
+
+
+//*********** 12-m **********
+
+// let n=+prompt('"N" son kiriting');
+
+// for (let i = 1; i <= n; i++ ){
+//      if(i%2==0)
+//      console.log(i);        
+// }
+
+
+//*********** 13-m **********
+
+// let n=+prompt('"N" son kiriting');
+
+// for (let i = 1; i <= n; i++ ){
+
+//      if(i == 1 || i == 2 || i%2==1)
+//      continue     
+//      console.log(i);        
+// }
+
+
+//*********** 14-m **********
+
+// let n=+prompt('"N" son kiriting');
+
+// for (let i = 1; i <= n; i++ ){     
+//      if(i%2==0){
+//           console.log(`Juft ${i**2}`);
+//      }else{
+//           console.log(`Toq ${i**2}`);  
+//      }
+          
+// }
+
+
+//*********** 15-m **********
+
+// let n=+prompt('"N" son kiriting');
+
+// for (let i = n; i >= 1; i-- ){    
+     
+//      console.log(i);
+// }
+
+
+//*********** 16-m **********
+
+// let n=+prompt('"N" son kiriting');
+
+// for (let i = 0; i <= n; i++ ){    
+//      if(i%5==0)
+//      continue
+//      console.log(i);
+// }
+
+
+//*********** 17-m **********
+
+// let n=+prompt('"N" son kiriting');
+// let sum=1;
+
+// for (let i = 1; i <= n; i++ ){
+//      sum*=i        
+// }
+// console.log(`N gacha butun sonlar ko'paytmasi ${sum} ga teng`);
+
+
+//*********** 18-m **********
+// TUSHUNMADIM
+
+//*********** 19-m **********
+// TUSHUNMADIM
+
+
